94ba78dd03347b6b549b43c8a60d448f25e27800
truedev0420 <slobodandeveloper20@gmail.com>
2023-02-22 04:17:01 +0900

    Dashboard updated with Earning

--- app/Http/Controllers/dashboard/Analytics.php
+++ app/Http/Controllers/dashboard/Analytics.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers\dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Auth;
 use App\Models\Hotspot;
+use App\Models\MonthlyEarning;
 use GuzzleHttp;
+use Auth;
 
 class Analytics extends Controller
 {
@@ -30,6 +31,14 @@ class Analytics extends Controller
     $year = date('Y');
     $last_month = date('m');
     
+
+    // Get Current Monthly_Earning from Database
+    $monthlyEarningDB = array($last_month);
+    for($month = 0; $month <= $last_month; $month++){
+      $monthlyEarningDB[$month] = MonthlyEarning::where("user_id", "=", Auth::user()->id)->where("during", "=", $year . '-'. $month)->first();
+    }
+
+
     $monthlyEarning = array($last_month);
     
     for($i = 0; $i < $last_month + 1; $i++)
@@ -42,18 +51,25 @@ class Analytics extends Controller
       $url ='https://api.helium.io/v1/hotspots/' 
         . $hotspot["address"];
         
-        $hotspots[$key]["status"] = json_decode($client->request('GET', $url, [
-          'headers' => [
-              'User-Agent' => $_SERVER['HTTP_USER_AGENT'],
-          ]
-        ])->getBody()->getContents())->data->status->online;
+      $hotspots[$key]["status"] = json_decode($client->request('GET', $url, [
+        'headers' => [
+            'User-Agent' => $_SERVER['HTTP_USER_AGENT'],
+        ]
+      ])->getBody()->getContents())->data->status->online;
 
-        if($hotspots[$key]["status"] === 'online')
-            $hotspots_online++;
+      if($hotspots[$key]["status"] === 'online')
+          $hotspots_online++;
+
 
       // Get Sum Monthly Earnings
       
       for($month = 1; $month <= $last_month; $month++){
+
+        // If TimeStamp Diff is less than 60, Fetch HotSpot API
+        if($monthlyEarningDB[$month] && ($month < $last_month || (strtotime(date("Y-m-d H:i:s")) - strtotime($monthlyEarningDB[$month]->create_at)) < 30)){
+          continue;
+        }
+          
 
         $min_time = date("Y-m-d", strtotime($year . '-' . $month . '-01'));
         $max_time = date("Y-m-t", strtotime($year . '-' . $month));
@@ -72,6 +88,27 @@ class Analytics extends Controller
       }
     }
     
+
+    // Save Fetched Hotspot Data to database
+    for($month = 1; $month <= $last_month; $month++){
+      if(!$monthlyEarningDB[$month]){
+        $monthly_earning = new MonthlyEarning();
+        $monthly_earning->user_id = Auth::user()->id;
+        $monthly_earning->during = $year . '-' . $month;
+        $monthly_earning->amount = $monthlyEarning[$month];
+        $monthly_earning->created_at = date("Y-m-d H:i:s");
+        $monthly_earning->save();
+      }else if($monthlyEarningDB[$month] && $monthlyEarning[$month] != 0 && $monthlyEarningDB[$month]->amount != $monthlyEarning[$month]){
+        $monthlyEarningDB[$month]->amount = $monthlyEarning[$month];
+        $monthlyEarningDB[$month]->created_at = date("Y-m-d H:i:s");
+        $monthlyEarningDB[$month]->save();
+      }
+
+      if($monthlyEarning[$month] == 0)
+        $monthlyEarning[$month] = floatval($monthlyEarningDB[$month]->amount);
+    }
+
+
     if(count($hotspots) != 0)
       $hotspots_online = number_format($hotspots_online / count($hotspots) * 100, 2, '.', '');
     else
